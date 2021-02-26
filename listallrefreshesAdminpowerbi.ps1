74b068664cbf9a191ee9524c0a86c840403d425c
NBK-Born <79723841+NBK-Born@users.noreply.github.com>
2021-02-26 16:04:22 -0500

    Add files via upload

---
+++ listallrefreshesAdminpowerbi.ps1
@@ -0,0 +1,17 @@
+﻿Login-PowerBI 
+
+
+$result = Invoke-PowerbiRestMethod -Url "https://api.powerbi.com/v1.0/myorg/admin/capacities/refreshables?%24top=5000" -Method Get 
+ 
+ForEach($item in  $result ){
+    Write-Output $item
+}
+
+
+
+
+$result = Invoke-PowerbiRestMethod -Url "https://api.powerbi.com/v1.0/myorg/admin/groups?%24expand=reports,datasets,dashboards,users&%24top=5000" -Method Get 
+ 
+ForEach($item in  $result ){
+    Write-Output $item
+}
---
+++ listreportsdatasetsdashboardsuserspowerbi.ps1
@@ -0,0 +1,10 @@
+﻿Login-PowerBI 
+
+ 
+
+ 
+$result = Invoke-PowerbiRestMethod -Url "https://api.powerbi.com/v1.0/myorg/admin/groups?%24expand=reports,datasets,dashboards,users&%24top=5000" -Method Get 
+ 
+ForEach($item in  $result ){
+    Write-Output $item
+}
